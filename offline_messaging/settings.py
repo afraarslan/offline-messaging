4982fedb4ce0d3ae7c64753eda32fdd7d80f5cd1
Afra Akbaş <akbasafra.aa@gmail.com>
2020-09-10 01:18:16 +0300

    remove django-nose

--- offline_messaging/settings.py
+++ offline_messaging/settings.py
@@ -40,18 +40,8 @@ INSTALLED_APPS = [
     'authentication',
     'messaging',
     'user_logging',
-    'django_nose',
 ]
 
-
-# Use nose to run all tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=messaging,authentication',
-]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
